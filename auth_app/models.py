6a1bf62ff61591ba978178ff42249c2cd27f347f
AymanElS4 <152307107+AymanElS4@users.noreply.github.com>
2025-12-25 13:36:27 -0500

    Creación de clase tag y relación con producto

--- auth_app/models.py
+++ auth_app/models.py
@@ -11,12 +11,42 @@ class User(models.Model):
     class Meta:
         db_table = "users"
 
+class Tag(models.Model):
+    TAG_CHOICES = [
+        ("gaming", "Gaming"),
+        ("laptop", "Laptop"),
+        ("pc", "PC"),
+        ("celular", "Celular"),
+        ("tablet", "Tablet"),
+        ("accesorio", "Accesorio"),
+        ("ssd", "SSD"),
+        ("ram", "RAM"),
+    ]
+
+    name = models.CharField(
+        max_length=50,
+        choices=TAG_CHOICES,
+        unique=True
+    )
+
+    def __str__(self):
+        return self.get_name_display()
+
+    class Meta:
+        db_table = "tags"
+
 class Product(models.Model):
     id_product = models.CharField(max_length = 100)
     name_product = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
-    vendor = models.ForeignKey(User, on_delete=models.CASCADE)
+    vendor = models.ForeignKey(User, on_delete=models.CASCADE, related_name="products")
+    tags = models.ManyToManyField(
+        Tag,
+        related_name="products",
+        blank=True
+    )
     class Meta:
         db_table = "products"
+
